cca3e2dc8612c16fbdeaa721d4743b46174e80ed
Matteo-Penaud <matteo.penaud@ynov.com>
2022-11-20 18:13:45 +0100

    First commit

---
+++ Core/Inc/ws2812b.h
@@ -0,0 +1,57 @@
+/*
+ * ws2812b.h
+ *
+ *  Created on: Nov 17, 2022
+ *      Author: matt
+ */
+
+#ifndef INC_WS2812B_H_
+#define INC_WS2812B_H_
+
+#include "stm32l4xx_hal.h"
+#include "malloc.h"
+
+#define WS2812B_MAX_PWM 26
+#define WS2812B_MIN_PWM 14
+
+typedef struct {
+	uint8_t red;
+	uint8_t green;
+	uint8_t blue;
+	uint8_t alpha;
+}t_s_rgbaColor;
+
+typedef struct {
+	uint32_t ledId;
+	uint32_t *next;
+	uint32_t *prev;
+	t_s_rgbaColor rgbaColor;
+}t_s_ws2812b_led;
+
+typedef struct {
+	uint32_t *prev;
+	uint32_t *next;
+	TIM_HandleTypeDef *htim;
+	uint32_t timerChannel;
+	uint32_t id;
+	uint32_t stripSize;
+	t_s_ws2812b_led *firstLed;
+}t_s_ws2812b_strip;
+
+typedef struct {
+	t_s_ws2812b_strip *entryPoint;
+	t_s_ws2812b_strip *tailPoint;
+}t_s_ws2812b_sequencer;
+
+extern uint8_t uc_dataSentFlag;
+
+void ws2812b_Init( void );
+void ws2812b_createStrip( TIM_HandleTypeDef *htim, uint32_t timerChannel, uint32_t stripSize );
+void ws2812b_createLeds( uint32_t stripId );
+void ws2812b_resetLeds( uint32_t stripId );
+t_s_ws2812b_led   * ws2812b_findLedById();
+t_s_ws2812b_strip * ws2812b_findStripById( uint32_t stripId );
+void ws2812b_generatePWMArray( uint32_t stripId, uint32_t *result );
+void ws2812b_sendStrip( uint32_t stripId );
+
+#endif /* INC_WS2812B_H_ */
---
+++ Core/Src/ws2812b.c
@@ -0,0 +1,180 @@
+/*
+ * ws2812b.c
+ *
+ *  Created on: Nov 17, 2022
+ *      Author: matt
+ */
+
+#include "ws2812b.h"
+
+uint8_t uc_dataSentFlag = 0;
+
+t_s_ws2812b_sequencer mainSequencer =
+	{ .entryPoint = NULL, .tailPoint = NULL };
+
+void ws2812b_createStrip( TIM_HandleTypeDef *htim, uint32_t timerChannel, uint32_t stripSize )
+{
+	static uint32_t stripId;
+	t_s_ws2812b_strip *cursor = NULL;
+
+	if(mainSequencer.entryPoint == NULL)
+	{
+		stripId = 0;
+		mainSequencer.entryPoint = (t_s_ws2812b_strip *)malloc(sizeof(t_s_ws2812b_strip));
+		mainSequencer.tailPoint = mainSequencer.entryPoint;
+
+		mainSequencer.entryPoint->next = NULL;
+		mainSequencer.entryPoint->prev = NULL;
+
+		cursor = mainSequencer.entryPoint;
+	}
+	else
+	{
+		cursor = mainSequencer.tailPoint;
+
+		cursor->next = (uint32_t *)malloc(sizeof(t_s_ws2812b_strip));
+		cursor = (t_s_ws2812b_strip *)cursor->next;
+		cursor->next = (uint32_t *)mainSequencer.entryPoint;
+		cursor->prev = (uint32_t *)mainSequencer.tailPoint;
+		mainSequencer.tailPoint = cursor;
+	}
+
+	cursor->id = stripId;
+	cursor->htim = htim;
+	cursor->timerChannel = timerChannel;
+	cursor->stripSize = stripSize;
+
+	ws2812b_createLeds(stripId);
+
+	/* WTF ? */
+//	cursor->next = NULL;
+//	cursor->prev = NULL;
+
+	stripId++;
+}
+
+void ws2812b_createLeds( uint32_t stripId )
+{
+	t_s_ws2812b_strip	*stripCursor = ws2812b_findStripById(stripId);
+	t_s_ws2812b_led 	*ledCursor = NULL;
+
+	uint32_t ledIndex = 0;
+
+	while(ledIndex < stripCursor->stripSize)
+	{
+		if(ledIndex == 0)
+		{
+			stripCursor->firstLed = (t_s_ws2812b_led *)malloc(sizeof(t_s_ws2812b_led));
+			ledCursor = stripCursor->firstLed;
+
+			ledCursor->next = (uint32_t *)ledCursor;
+			ledCursor->prev = (uint32_t *)ledCursor;
+		}
+		else
+		{
+			ledCursor = (t_s_ws2812b_led *)stripCursor->firstLed->prev;
+
+			ledCursor->next = (uint32_t *)malloc(sizeof(t_s_ws2812b_led));
+			ledCursor = (t_s_ws2812b_led *)ledCursor->next;
+			ledCursor->next = (uint32_t *)stripCursor->firstLed;
+			ledCursor->prev = (uint32_t *)stripCursor->firstLed->prev;
+			stripCursor->firstLed->prev = (uint32_t *)ledCursor;
+		}
+
+		ledCursor->ledId = ledIndex;
+		ledCursor->rgbaColor.red = 0;
+		ledCursor->rgbaColor.green = 0;
+		ledCursor->rgbaColor.blue = 0;
+		ledCursor->rgbaColor.alpha = 0;
+
+		ledIndex++;
+	}
+}
+
+void ws2812b_resetLeds( uint32_t stripId )
+{
+	t_s_ws2812b_strip	*stripCursor = ws2812b_findStripById(stripId);
+	t_s_ws2812b_led 	*ledCursor = stripCursor->firstLed;
+
+	while(ledCursor->ledId < stripCursor->stripSize)
+	{
+		ledCursor->rgbaColor.red = 0;
+		ledCursor->rgbaColor.green = 0;
+		ledCursor->rgbaColor.blue = 0;
+		ledCursor->rgbaColor.alpha = 0;
+
+		ledCursor = (t_s_ws2812b_led *)ledCursor->next;
+	}
+}
+
+t_s_ws2812b_strip * ws2812b_findStripById( uint32_t stripId )
+{
+	t_s_ws2812b_strip *cursor;
+
+	if((mainSequencer.tailPoint->id / 2) < stripId)
+	{
+		cursor = mainSequencer.tailPoint;
+
+		while(cursor->id != stripId)
+		{
+			cursor = (t_s_ws2812b_strip *)cursor->prev;
+		}
+	}
+	else
+	{
+		cursor = mainSequencer.entryPoint;
+
+		while(cursor->id != stripId)
+		{
+			cursor = (t_s_ws2812b_strip *)cursor->next;
+		}
+	}
+
+	return cursor;
+}
+
+void ws2812b_generatePWMArray( uint32_t stripId, uint32_t *result )
+{
+	t_s_ws2812b_strip strip = *ws2812b_findStripById(stripId);
+	t_s_ws2812b_led *cursor = strip.firstLed;
+
+	for(uint32_t led = 0; led < strip.stripSize; led++)
+	{
+		for(uint8_t i = 0; i < 8; i++)
+		{
+			if(cursor->rgbaColor.green & (1 << (7-i)))
+				result[(24*led)+i] = WS2812B_MAX_PWM;
+			else
+				result[(24*led)+i] = WS2812B_MIN_PWM;
+		}
+
+		for(uint8_t i = 0; i < 8; i++)
+		{
+			if(cursor->rgbaColor.red & (1 << (7-i)))
+				result[(24*led)+i+8] = WS2812B_MAX_PWM;
+			else
+				result[(24*led)+i+8] = WS2812B_MIN_PWM;
+		}
+
+		for(uint8_t i = 0; i < 8; i++)
+		{
+			if(cursor->rgbaColor.blue & (1 << (7-i)))
+				result[(24*led)+i+16] = WS2812B_MAX_PWM;
+			else
+				result[(24*led)+i+16] = WS2812B_MIN_PWM;
+		}
+	}
+
+	for(uint32_t i=0; i < 50; i++)
+		result[i+(24*strip.stripSize)] = 0;
+}
+
+void ws2812b_sendStrip( uint32_t stripId )
+{
+	t_s_ws2812b_strip *stripCursor = ws2812b_findStripById(stripId);
+	uint32_t stripToSend[(24*stripCursor->stripSize)+50];
+
+	ws2812b_generatePWMArray(stripId, stripToSend);
+
+	HAL_TIM_PWM_Start_DMA(stripCursor->htim, stripCursor->timerChannel, (uint32_t *)&stripToSend, (24*stripCursor->stripSize)+50);
+}
